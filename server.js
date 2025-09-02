7523260a788fb46e75fba72efab8530d725290b4
tanisingh134 <148522117+tanisingh134@users.noreply.github.com>
2025-09-02 12:18:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'https://advancechatapplication.onrender.com/',
+        origin: 'http://localhost:3000',
         methods: ['GET', 'POST'],
         credentials: true,
     },
@@ -294,4 +294,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
 
